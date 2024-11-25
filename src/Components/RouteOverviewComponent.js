4069f8ed7d2bac9a9e4f251adca451b755f62aa9
timosbonus <sutter.timo1998@gmail.com>
2024-11-25 16:08:45 +0100

    Edited Modal to be Able to enter Fields for new Route

--- src/Components/RouteOverviewComponent.js
+++ src/Components/RouteOverviewComponent.js
@@ -5,12 +5,38 @@ import Modal from "./Modal";
 function RouteOverviewComponent({ routeId, setRouteId }) {
   const [isModalOpen, setModalOpen] = useState(false); // state for current modal state
 
+  function handleNewRouteInformation() {
+    setModalOpen(false);
+  }
+
   return (
     <>
       <Navbar routeId={routeId} setModalOpen={setModalOpen}></Navbar>
       <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)}>
-        <h2>Modal Title</h2>
-        <p>This is a simple modal.</p>
+        <div>
+          <h2>Add new Route</h2>
+          <p>Add all relevant Information below</p>
+          <form onSubmit={handleNewRouteInformation}>
+            <div>
+              <label>Routenname</label>
+              <input type="text" id="routeId"></input>
+            </div>
+            <div>
+              <label>Break</label>
+              <input type="number" id="breakDuration"></input>
+            </div>
+            <div>
+              <label>Start Time</label>
+              <input type="time" id="startTime"></input>
+            </div>
+          </form>
+          <button
+            onClick={handleNewRouteInformation}
+            style={{ marginTop: "20px" }}
+          >
+            Save new Route
+          </button>
+        </div>
       </Modal>
     </>
   );
