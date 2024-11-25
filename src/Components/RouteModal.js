36d0de25181c38fae8a9e10abced2d6260a92656
timosbonus <sutter.timo1998@gmail.com>
2024-11-25 16:35:42 +0100

    Added standard values for RouteModal

---
+++ src/Components/RouteModal.js
@@ -0,0 +1,54 @@
+import { useRef } from "react";
+
+function RouteModal({ children, handleNewRouteInformation, routeId }) {
+  // refs for standard values
+  const newRouteId = useRef("");
+  const breakDuration = useRef(20);
+  const startTime = useRef("07:00");
+
+  if (routeId) {
+    // checks if routeId is false
+    newRouteId.current = routeId.routeId;
+    breakDuration.current = routeId.breakDuration;
+    startTime.current = routeId.startTime;
+  }
+
+  return (
+    <div>
+      {children}
+      <form>
+        <div>
+          <label>Routenname</label>
+          <input type="text" id="routeId" defaultValue={newRouteId.current} />
+        </div>
+        <div>
+          <label>Break</label>
+          <input
+            type="number"
+            id="breakDuration"
+            defaultValue={breakDuration.current}
+            ref={breakDuration} // ref für Break
+          />
+        </div>
+        <div>
+          <label>Start Time</label>
+          <input
+            type="time"
+            id="startTime"
+            defaultValue={startTime.current}
+            ref={startTime} // ref für Startzeit
+          />
+        </div>
+        <button
+          type="submit"
+          style={{ marginTop: "20px" }}
+          onClick={() => handleNewRouteInformation()}
+        >
+          Save new Route
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default RouteModal;
--- src/Components/RouteOverviewComponent.js
+++ src/Components/RouteOverviewComponent.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Navbar from "./Navbar";
 import Modal from "./Modal";
+import RouteModal from "./RouteModal";
 
 function RouteOverviewComponent({ routeId, setRouteId }) {
   const [isModalOpen, setModalOpen] = useState(false); // state for current modal state
 
-  function handleNewRouteInformation() {
+  function handleNewRouteInformation(formData) {
+    setRouteId(formData);
     setModalOpen(false);
   }
 
@@ -13,30 +15,13 @@ function RouteOverviewComponent({ routeId, setRouteId }) {
     <>
       <Navbar routeId={routeId} setModalOpen={setModalOpen}></Navbar>
       <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)}>
-        <div>
+        <RouteModal
+          handleNewRouteInformation={handleNewRouteInformation}
+          routeId={routeId}
+        >
           <h2>Add new Route</h2>
           <p>Add all relevant Information below</p>
-          <form onSubmit={handleNewRouteInformation}>
-            <div>
-              <label>Routenname</label>
-              <input type="text" id="routeId"></input>
-            </div>
-            <div>
-              <label>Break</label>
-              <input type="number" id="breakDuration"></input>
-            </div>
-            <div>
-              <label>Start Time</label>
-              <input type="time" id="startTime"></input>
-            </div>
-          </form>
-          <button
-            onClick={handleNewRouteInformation}
-            style={{ marginTop: "20px" }}
-          >
-            Save new Route
-          </button>
-        </div>
+        </RouteModal>
       </Modal>
     </>
   );
