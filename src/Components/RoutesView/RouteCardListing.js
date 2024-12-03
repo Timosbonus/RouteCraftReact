c2e0a73836f93afd614cfdff6ead2009235e6916
timosbonus <sutter.timo1998@gmail.com>
2024-12-03 14:30:12 +0100

    Finished Card Appearance and added two buttons to delete and edit them

--- src/Components/RoutesView/RouteCardListing.js
+++ src/Components/RoutesView/RouteCardListing.js
@@ -10,7 +10,7 @@ function RouteCardListing({ handleSetRouteInformation }) {
   // useEffect to get all Routes from Backend
   useEffect(() => {
     fetchRoutes();
-  }, []);
+  }, [allRoutes]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -32,7 +32,7 @@ function RouteCardListing({ handleSetRouteInformation }) {
   }
 
   function deleteRouteByButtonClick(routeId) {
-    deleteSelectedRoute(routeId).then((deletedRoute) => {
+    deleteSelectedRoute(routeId).then(() => {
       fetchRoutes();
     });
   }
@@ -40,18 +40,35 @@ function RouteCardListing({ handleSetRouteInformation }) {
   return (
     <div className="route-list">
       {allRoutes.map((r) => (
-        <div
-          key={r.routeId}
-          className="route-card"
-          onClick={() => handleSetRouteInformation(r)}
-        >
-          <div className="route-card-header">
-            <h3>{r.routeId}</h3>
-          </div>
-          <div className="route-card-body">
-            <p>Start Time: {r.startTime}</p>
-            <p>Default Break Time: {r.defaultBreakDuration} min</p>{" "}
-            {/* Default Break Time */}
+        <div key={r.routeId} className="route-card">
+          <div className="route-card-content">
+            <div className="route-card-text">
+              <h3>{r.routeId}</h3>
+              <p>Start Time: {r.startTime} Uhr</p>
+              <p>Break Time: {r.defaultBreakDuration} min</p>
+            </div>
+            <div className="route-card-buttons">
+              <button
+                onClick={() => handleSetRouteInformation(r)}
+                className="icon-button"
+              >
+                <img
+                  src={`${process.env.PUBLIC_URL}/assets/editing.png`}
+                  alt="Edit"
+                  className="icon-button-image"
+                />
+              </button>
+              <button
+                onClick={() => deleteRouteByButtonClick(r.routeId)}
+                className="icon-button"
+              >
+                <img
+                  src={`${process.env.PUBLIC_URL}/assets/bin.png`}
+                  alt="Delete"
+                  className="icon-button-image"
+                />
+              </button>
+            </div>
           </div>
         </div>
       ))}
